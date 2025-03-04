cdd819ef5097019229d7c0dbbac52bc8e8a6b683
lucas pedroso <pedrosolucas1745@gmail.com>
2025-03-04 20:49:12 -0300

    feat: create a class JWTObject to manipulate and build a tokken

---
+++ src/main/java/dio/api_swagger/Security/JWTObject.java
@@ -0,0 +1,43 @@
+package dio.api_swagger.Security;
+
+import java.util.Date;
+import java.util.List;
+
+public class JWTObject {
+    private String subject;
+    private Date issuedAt; // date of token creation
+    private Date expiration;
+    private List<String> roles; // roles accses
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public Date getIssuedAt() {
+        return issuedAt;
+    }
+
+    public void setIssuedAt(Date issuedAt) {
+        this.issuedAt = issuedAt;
+    }
+
+    public Date getExpiration() {
+        return expiration;
+    }
+
+    public void setExpiration(Date expiration) {
+        this.expiration = expiration;
+    }
+
+    public List<String> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(List<String> roles) {
+        this.roles = roles;
+    }
+}
