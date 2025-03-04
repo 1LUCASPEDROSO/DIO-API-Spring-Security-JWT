b0356689fda855da39bac6290fb82037828581c3
lucas pedroso <pedrosolucas1745@gmail.com>
2025-03-04 20:53:50 -0300

    feat: create a userService to encypt passowrd and save user

---
+++ src/main/java/dio/api_swagger/Service/userService.java
@@ -0,0 +1,19 @@
+package dio.api_swagger.Service;
+
+import dio.api_swagger.Entity.User;
+import dio.api_swagger.Repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+public class userService {
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private PasswordEncoder encoder;
+    public void createUser(User user){
+        String pass = user.getPassword();
+        user.setPassword(encoder.encode(pass)); // encrypiting password
+        userRepository.save(user);
+    }
+}
