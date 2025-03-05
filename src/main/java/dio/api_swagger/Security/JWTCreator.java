ff64ddd0302e9f82430158c8e4f4295fc936c564
lucas pedroso <pedrosolucas1745@gmail.com>
2025-03-05 20:20:17 -0300

    refactor: refactor name of method to create token and method to deocde

--- src/main/java/dio/api_swagger/Security/JWTCreator.java
+++ src/main/java/dio/api_swagger/Security/JWTCreator.java
@@ -9,12 +9,12 @@ public class JWTCreator {
     public static final String HEADER_AUTHORIZATION = "Authorization";
     public static final String ROLES_AUTHORITIES = "authorities";
 
-    public static String create1(String prefix,String key, JWTObject jwtObject) {
+    public static String createToken(String prefix,String key, JWTObject jwtObject) {
         String token = Jwts.builder().setSubject(jwtObject.getSubject()).setIssuedAt(jwtObject.getIssuedAt()).setExpiration(jwtObject.getExpiration())
                 .claim(ROLES_AUTHORITIES, checkRoles(jwtObject.getRoles())).signWith(SignatureAlgorithm.HS512, key).compact();
         return prefix + " " + token;
     }
-    public static JWTObject create(String token,String prefix,String key)
+    public static JWTObject decode(String token,String prefix,String key)
             throws ExpiredJwtException, UnsupportedJwtException, MalformedJwtException, SignatureException {
         JWTObject object = new JWTObject();
         token = token.replace(prefix, "");
