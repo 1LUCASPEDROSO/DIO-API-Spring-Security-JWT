fc6de0f7a282297b4188391745fffbf60a031ced
lucas pedroso <pedrosolucas1745@gmail.com>
2025-03-05 20:19:33 -0300

    fix: add spring notation for serice class

--- src/main/java/dio/api_swagger/Service/userService.java
+++ src/main/java/dio/api_swagger/Service/userService.java
@@ -4,7 +4,9 @@ import dio.api_swagger.Entity.User;
 import dio.api_swagger.Repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
 
+@Service
 public class userService {
     @Autowired
     private UserRepository userRepository;
