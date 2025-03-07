c970c1be298384b9768f0e5879ba019e91e1b186
lucas pedroso <pedrosolucas1745@gmail.com>
2025-03-06 21:06:05 -0300

    reafctor: add JWTFilter using object new routes for Get user and Post for new users

--- src/main/java/dio/api_swagger/Config/WebSecurityConfig.java
+++ src/main/java/dio/api_swagger/Config/WebSecurityConfig.java
@@ -1,28 +1,17 @@
 package dio.api_swagger.Config;
 
 import dio.api_swagger.Security.JWTFilter;
-import dio.api_swagger.Security.SecurityDatabaseService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-
-import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
@@ -33,6 +22,8 @@ public class WebSecurityConfig {
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder(); // Aqui estamos criando o PasswordEncoder
     }
+    @Autowired
+    JWTFilter jwtFilter;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -41,11 +32,12 @@ public class WebSecurityConfig {
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/").permitAll()
                         .requestMatchers(HttpMethod.POST, "/login").permitAll()
+                        .requestMatchers(HttpMethod.POST,"/user").permitAll()
+                        .requestMatchers(HttpMethod.GET,"/user").hasAnyRole("USERS", "MANAGERS")
                         .requestMatchers("/manager").hasRole("MANAGERS")
-                        .requestMatchers("/user").hasAnyRole("USERS", "MANAGERS")
                         .anyRequest().authenticated()
                 )
-                .addFilterAfter(new JWTFilter(), UsernamePasswordAuthenticationFilter.class); // Adiciona o JWTFilter
+                .addFilterAfter(jwtFilter, UsernamePasswordAuthenticationFilter.class); // Adiciona o JWTFilter
         return http.build();
     }
 }
