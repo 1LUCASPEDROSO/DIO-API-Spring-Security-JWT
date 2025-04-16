6241e038af4c187c229dc7b9d98bdfcb5981e81e
lucas pedroso <pedrosolucas1745@gmail.com>
2025-04-16 02:12:00 -0300

    feat: create new service for users to get roles

--- src/main/java/dio/api_swagger/Service/UserService.java
+++ src/main/java/dio/api_swagger/Service/UserService.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class UserService {
     @Autowired
@@ -17,5 +19,9 @@ public class UserService {
         String pass = user.getPassword();
         user.setPassword(encoder.encode(pass)); // encrypiting password
         userRepository.save(user);
+    }
+    public List<String> getRoles(String username){
+       User user = userRepository.findByUsername(username);
+        return user.getRoles();
     }
 }
